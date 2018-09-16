0f320948aad5937989ce9bbd582cfc5db5b271d6
Pedro Ferrari <pedro@pedroferrari.com>
2018-09-16 04:25:14 +0200

    Install image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,17 @@ module.exports = {
 		title: 'Pedro Ferrari Web Developer',
 		description: 'Website Description'
 	},
-	plugins: [ 'gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components' ]
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-styled-components',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'img',
+				path: `${__dirname}/src/images/`
+			}
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp'
+	]
 };
